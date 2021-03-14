67999f3d5e4254ae2798eb671641017a16b18ad8
Joao Paulo Albuquerque <joao.2019paulo.2020@gmail.com>
2021-03-14 10:55:25 -0300

    Convertendo numeros

--- Praticando/conversoes_numericas/Conversoes.java
+++ Praticando/conversoes_numericas/Conversoes.java
@@ -2,6 +2,18 @@ package conversoes_numericas;
 
 public class Conversoes{
     public static void main(String[] args){
+
+        //CONVERTENDO DE INT PARA FLOAT
+        int n1 = 10;
+        float n2 = n1;
         
+        //CONVERTENDO DE FLOAT PARA INT
+        float n3 = 15.5f;
+        int n4 = (int) n3;
+
+        //CONVERTENDO DO INT PARA STRING
+        int n5 = 10;
+        String texto = Integer.toString(n5);
+
     }
 }
