6da25672c6196d75fb9eb3ca82339e5ec4e18454
Joao Paulo Albuquerque <joao.2019paulo.2020@gmail.com>
2021-03-16 19:47:03 -0300

    Classe Scanner

--- Praticando/classe_scanner/LendoDoTeclado.java
+++ Praticando/classe_scanner/LendoDoTeclado.java
@@ -1,8 +1,26 @@
 package classe_scanner;
 
 import java.util.*;
+import java.math.*;
 
 public class LendoDoTeclado {
-    Scanner teclado = new Scanner(System.in);
-    
+    public static void main(String[] args){
+        
+        Scanner teclado = new Scanner(System.in);
+
+        String nome = "";
+        float n1 = 0;
+        float n2 = 0;
+
+        System.out.print("Digite seu nome: ");
+        nome = teclado.nextLine();
+        System.out.print("Digite um numero: ");
+        n1 = teclado.nextFloat();
+        System.out.print("Digite outro numero: ");
+        n2 = teclado.nextFloat();
+
+        float maior = (n1 + n2 + Math.abs(n1 - n2)) / 2;
+
+        System.out.format("Olá %s, o maior numero foi %.2f!\n", nome, maior);
+    }
 }
