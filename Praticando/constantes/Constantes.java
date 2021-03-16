ce1ad686f6e56c9d7845951a8d36521baa946e3e
Joao Paulo Albuquerque <joao.2019paulo.2020@gmail.com>
2021-03-16 19:52:06 -0300

    Constantes

---
+++ Praticando/constantes/Constantes.java
@@ -0,0 +1,14 @@
+package constantes;
+
+public class Constantes {
+    public static void main(String[] args){
+
+        /*DIFERENTE DE UMA VARIÁVEL, UMA CONSTANTE
+         *NAO PODE TER SEU VALOR ALTERANDO AO TEMPO DE EXECUÇÃO*/
+
+        //DECLARANDO A CONSTANTE PI
+        final float PI = 3.14f;
+
+        System.out.println("Constante PI: " + PI);
+    }
+}
