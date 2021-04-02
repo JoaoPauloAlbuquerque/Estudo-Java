2e3983be1e350d86aa4ad05c56d197270938ab9d
Joao Paulo Albuquerque <joao.2019paulo.2020@gmail.com>
2021-04-02 20:09:38 -0300

    Update repository

--- Praticando/basico/primeiro_programa/OlaMundo.java
+++ Praticando/basico/primeiro_programa/OlaMundo.java
@@ -1,6 +1,7 @@
 package basico.primeiro_programa;
 public class OlaMundo{
     public static void main(String[] args){
+        //IMPRIMINDO NA TELA "Olá, mundo!"
         System.out.println("Olá, mundo!");
     }
 }
